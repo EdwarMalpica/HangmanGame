70eb99a39f711e9f585c8d6f1d083f793d419e75
Edwar Malpica <edwar.malpica@uptc.edu.co>
2023-02-12 22:33:12 -0500

    Adiccion clase jugador

--- view/MainView.py
+++ view/MainView.py
@@ -1,5 +1,6 @@
 import pygame, sys
-pygame.init()
+
+from view.Player import Player
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -7,17 +8,41 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
-size = (800, 500)
-screen = pygame.display.set_mode(size)
-clock = pygame.time.Clock()
+class MainView:
+    def __init__(self):
+        self.game = pygame.init()
+        self.screen = pygame.display.set_mode((700,512))
+        self.clock = pygame.time.Clock()
+        self.player = Player()
+        self.game_over = False
+        self.background = pygame.image.load("assets/img/FondoJuego.png").convert()
+        self.sounBack = pygame.mixer.Sound("assets/sound/Back.mp3")
 
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+    def mainLoop(self):
+        self.sounBack.play()
+        while not self.game_over:
+            self.eventsControl()
+            self.initComponents()
+            print(pygame.mouse.get_pos())
+        pygame.quit()
+
+    def eventsControl(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.player.addArm()
+
+    def initComponents(self):
+        self.screen.blit(self.background, [0, 0])
+        self.player.drawPlayer(self.screen)
+        pygame.display.flip()
+        self.clock.tick(30)
 
 
-    screen.fill(WHITE)
-    pygame.display.flip()
-    clock.tick(30)
+
+mainVies = MainView()
+mainVies.mainLoop()
+
