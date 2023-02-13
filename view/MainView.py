c3c839a78f1b4e9c51d50854c5ce2c9691a0762d
Edwar Malpica <edwar.malpica@uptc.edu.co>
2023-02-12 22:49:45 -0500

    Documentacion de clase MainView

--- view/MainView.py
+++ view/MainView.py
@@ -8,7 +8,18 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
+
 class MainView:
+
+    """
+    game: inicializa Pygame.
+    screen: establece el tamaño de la ventana del juego en 700x512 píxeles.
+    clock: controla la tasa de refresco del juego.
+    player: un objeto de la clase Player que se utiliza para dibujar el jugador.
+    game_over: una bandera que indica si el juego ha terminado.
+    background: la imagen de fondo que se muestra en la ventana del juego.
+    sounBack: el sonido de fondo que se reproducirá mientras el juego esté en curso.
+    """
     def __init__(self):
         self.game = pygame.init()
         self.screen = pygame.display.set_mode((700,512))
@@ -19,14 +30,21 @@ class MainView:
         self.sounBack = pygame.mixer.Sound("assets/sound/Back.mp3")
 
 
+    """ 
+         este es el bucle principal del juego. Mientras la bandera "game_over"
+          sea falsa, se llamarán los métodos eventsControl y "initComponents.
+    """
     def mainLoop(self):
-        self.sounBack.play()
+        self.sounBack.play(loops=20)
         while not self.game_over:
             self.eventsControl()
             self.initComponents()
             print(pygame.mouse.get_pos())
         pygame.quit()
 
+    """
+    controla los eventos que ocurren en el juego, como cerrar la ventana o presionar una tecla.
+    """
     def eventsControl(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -35,6 +53,9 @@ class MainView:
                 if event.key == pygame.K_UP:
                     self.player.addArm()
 
+    """
+    dibuja la imagen de fondo y el jugador en la pantalla y actualiza el juego.
+    """
     def initComponents(self):
         self.screen.blit(self.background, [0, 0])
         self.player.drawPlayer(self.screen)
