beeebbd40e48d64b57e31d365a1b5feebd9647fd
EstebanRincon12 <esteban.rincon@uptc.edu.co>
2023-02-15 20:34:44 -0500

    Obtener lista de jugadores

--- model/db_conector.py
+++ model/db_conector.py
@@ -5,21 +5,28 @@ try:
     cnxn = pyodbc.connect('DRIVER={SQL Server};Server=DESKTOP-P3OGUBB\SQLEXPRESS;Database=HangmanGame;Trusted_Connection=yes')
     print("funciono")
     cursor = cnxn.cursor()
-    cursor.execute("SELECT * FROM Players order by 3 DESC") 
+    listPlayer = []
 
+    cursor.execute("SELECT TOP 10 * FROM Players order by 3 DESC") 
     row = cursor.fetchone() 
     
 
     def createPlayer(name, score):
          cursor.execute(f"INSERT INTO Players VALUES ('{name}', {score})")
          cursor.commit()
-    createPlayer('Santiago', 25)
-
-    cursor.execute("SELECT * FROM Players order by 3 DESC")
+ 
+    
     while row:
-        print (row) 
+        print (row)
+        listPlayer.append(row)
         row = cursor.fetchone()
-    cursor.close() 
+
+
+    def getList():
+        return listPlayer        
+       
+    cursor.close()
+    
 except Exception as ex:
     print(ex)
     
